18eaaa35968a2bb809cf3788038140af99dd5cdb
viveka2024 <humbertocoaching@gmail.com>
2025-07-24 17:59:44 +0200

    Create config-fidelity.js lectura variables supabase

---
+++ api/config-fidelity.js
@@ -0,0 +1,12 @@
+// File: /api/config-fidelity.js
+export default function handler(req, res) {
+  // Sólo GET
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+  return res.status(200).json({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  });
+}
